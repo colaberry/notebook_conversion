e6e00d1f1d27fe99ed665744905d61da954f082d
Durwasa Chakraborty <durwasa@Durwasas-MacBook-Pro.local>
2019-03-20 15:08:48 -0400

    added decorators

--- html-links/app.py
+++ html-links/app.py
@@ -1,6 +1,17 @@
 import os
+import time
 
-def main():        
+
+def timeit(method):
+        """The timeit decorator"""
+        def running_time(*args, **kwargs):
+                start_time = time.time()
+                result = method(*args, **kwargs)
+                end_time = time.time()
+                print('%r %2.2f ms' % (method.__name__, (end_time - start_time) % 1000))
+        return running_time
+
+def convert_ipynb_html():
         # singleCommand = 'jupyter nbconvert --to html htmlFiles/{0}'
         multiplefileCommand = 'jupyter nbconvert '
         allFiles = ''
@@ -10,9 +21,8 @@ def main():
         for root, dirs, files in os.walk(directory):
                 for filename in files:
                         allFiles += directory+'/'+filename+' '
-                
         p=multiplefileCommand+allFiles
         os.system(p)
 
 if __name__ == "__main__":
-    main()
+    convert_ipynb_html()
